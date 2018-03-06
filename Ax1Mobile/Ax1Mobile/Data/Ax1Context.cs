55af68fa16527a3007d869f08689be3d999ab5bb
Luke Bethay <lukebethay@gmail.com>
2018-03-05 20:22:52 -0500

    Added FileHelper method & interface.

--- Ax1Mobile/Ax1Mobile/Data/Ax1Context.cs
+++ Ax1Mobile/Ax1Mobile/Data/Ax1Context.cs
@@ -8,23 +8,14 @@ namespace Ax1Mobile
     /// </summary>
     public class Ax1Context : DbContext
     {
+        #region Private implementation
         private readonly string _localdb;
-
-        /// <summary>
-        /// Cost centers dataset, a table of my cost centers.
-        /// </summary>
-        public DbSet<CostCenter> CostCenters { get; set; }
-
-        /// <summary>
-        /// Employees table that is created.
-        /// </summary>
-        //public DbSet<Employee> Employees { get; set; }
 
         /// <summary>
         /// Constructor 
         /// </summary>
         /// <param name="options"></param>
-        public Ax1Context(string localdb)
+        protected Ax1Context(string localdb)
         {
             _localdb = localdb;
 
@@ -35,11 +26,24 @@ namespace Ax1Mobile
         {
             optionsBuilder.UseSqlite(string.Format("Filename={0}", _localdb));
         }
+        #endregion
 
-        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        /// <summary>
+        /// Cost centers dataset, a table of my cost centers.
+        /// </summary>
+        public DbSet<CostCenter> CostCenters { get; set; }
+
+        /// <summary>
+        /// Employees table that is created.
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+
+        public static Ax1Context Create(string localdb)
         {
-            modelBuilder.Entity<CostCenter>().ToTable("CostCenter");
-            modelBuilder.Entity<Employee>().ToTable("Employee");
-        } */
+            var dbContext = new Ax1Context(localdb);
+            dbContext.Database.EnsureCreated();
+            dbContext.Database.Migrate();
+            return dbContext;
+        }
     }
 }
---
+++ Ax1Mobile/Ax1Mobile/IFileHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ax1Mobile
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
+}
